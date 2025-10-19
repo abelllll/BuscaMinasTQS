29294d80242c4a2950bd549827151450a2cfc1a7
Abel-1632248 <tucorreoexample.com>
2025-10-19 17:02:44 +0200

    TDD: test colocación de minas (cuenta exacta) - FALLANDO

--- src/test/java/com/abelg/buscaminas/Modelo/TableroTest.java
+++ src/test/java/com/abelg/buscaminas/Modelo/TableroTest.java
@@ -10,7 +10,7 @@ public class TableroTest {
     void testInicializacionDimensionesYCeldas() {
         int filas = 5, columnas = 6, minas = 0;
         Random rng = new Random(42); // Semilla fija para pruebas
-        tablero t = new tablero(filas, columnas, minas, rng);
+        Tablero t = new Tablero(filas, columnas, minas, rng);
 
         assertEquals(filas, t.getFilas(), "Filas incorrectas");
         assertEquals(columnas, t.getColumnas(), "Columnas incorrectas");
@@ -18,4 +18,20 @@ public class TableroTest {
         assertNotNull(t.getCasilla(filas-1, columnas-1), "Debe existir casilla (última)");
         // Aún no comprobamos minas ni adyacencias
     }
+
+    @Test
+    void testColocacionDeMinasCuentaExacta() {
+    int filas = 8, columnas = 8, minas = 10;
+    Random rng = new Random(7); // semilla fija para reproducibilidad
+    Tablero t = new Tablero(filas, columnas, minas, rng);
+
+    int totalMinas = 0;
+    for (int f = 0; f < filas; f++) {
+        for (int c = 0; c < columnas; c++) {
+            if (t.getCasilla(f,c).isMinada()) totalMinas++;
+        }
+    }
+    assertEquals(minas, totalMinas, "Debe haber exactamente N minas");
+    }
+
 }
